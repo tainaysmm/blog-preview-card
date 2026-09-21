0e2d0715952200c8a933a628c33a11eec213265b
taina yasmin <taina.yasmin@aluno.ifsp.edu.br>
2026-09-21 08:05:04 -0300

    site

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-<h1 className="text-3xl font-extrabold text-yellow-500">Helloo, World!</h1>
+<h1 className="text-3xl font-extrabold text-yellow-500">Hello, World!</h1>
     </>
   )
 }
